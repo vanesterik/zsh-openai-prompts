461c1be5a68fc4beb418efbfa5381cae3b41ba4e
Koen Dirk van Esterik <koen@vanesterik.com>
2023-06-23 14:19:05 +0200

    fix: add location variables to copy statement

--- zsh-openai-prompts.plugin.zsh
+++ zsh-openai-prompts.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-[ ! -f ~/.zsh-openai-prompts.config ] && cp zsh-openai-prompts.config.example ~/.zsh-openai-prompts.config
+[ ! -f ~/.zsh-openai-prompts.config ] && cp "${0:A:h}/zsh-openai-prompts.config.example" ~/.zsh-openai-prompts.config
 
 source ~/.config.zsh
 source "${0:A:h}/zsh-openai-prompts.zsh"
