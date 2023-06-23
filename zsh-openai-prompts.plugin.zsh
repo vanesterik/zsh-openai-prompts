851fce2c67301f3a67611d7638807815b08666e8
Koen Dirk van Esterik <koen@vanesterik.com>
2023-06-23 14:26:02 +0200

    fix: apply dry principle

--- zsh-openai-prompts.plugin.zsh
+++ zsh-openai-prompts.plugin.zsh
@@ -1,6 +1,8 @@
 #!/bin/zsh
 
-[ ! -f ~/.zsh-openai-prompts.config ] && cp "${0:A:h}/zsh-openai-prompts.config.example" ~/.zsh-openai-prompts.config
+config_file=~/.zsh-openai-prompts.config
 
-source ~/.config.zsh
+[ ! -f $config_file ] && cp "${0:A:h}/zsh-openai-prompts.config.example" $config_file
+
+source $config_file
 source "${0:A:h}/zsh-openai-prompts.zsh"
