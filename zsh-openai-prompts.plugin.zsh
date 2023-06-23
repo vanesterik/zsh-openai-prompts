028f41eafc4e6d4edfc0e694030c8cd869a064eb
Koen Dirk van Esterik <koen@vanesterik.com>
2023-06-23 14:17:05 +0200

    fix: apply some naming conventions part 2

--- zsh-openai-prompts.plugin.zsh
+++ zsh-openai-prompts.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-[ ! -f ~/.zsh-openai-prompts-config.zsh ] && cp .zsh-openai-prompts-config.zsh.example ~/.zsh-openai-prompts-config.zsh
+[ ! -f ~/.zsh-openai-prompts.config ] && cp zsh-openai-prompts.config.example ~/.zsh-openai-prompts.config
 
 source ~/.config.zsh
 source "${0:A:h}/zsh-openai-prompts.zsh"
