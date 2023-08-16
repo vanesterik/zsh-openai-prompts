8f0f5d2a647e588f4ea8754339b6838a9bb96c05
Koen Dirk van Esterik <koen@vanesterik.com>
2023-08-16 19:51:57 +0200

    fix: add zsh extension to example file

--- zsh-openai-prompts.plugin.zsh
+++ zsh-openai-prompts.plugin.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-config_file=~/.zsh-openai-prompts.config
+config_file=~/.zsh-openai-prompts.config.zsh
 
-[ ! -f $config_file ] && cp "${0:A:h}/zsh-openai-prompts.config.example" $config_file
+[ ! -f $config_file ] && cp "${0:A:h}/zsh-openai-prompts.config.example.zsh" $config_file
 
 source $config_file
 source "${0:A:h}/zsh-openai-prompts.zsh"
